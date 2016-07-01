a4baca510ca3fe51c7dfbdc0a10a5a8c5e79a8ce
prz3m <prz3m@users.noreply.github.com>
2016-07-01 16:06:31 +0200

    kind2anki - import words from Kindle's Vocabulary Builder and automatically translate

---
+++ kind2anki/__init__.py
@@ -0,0 +1,101 @@
+# coding=utf-8
+# anki stuff import
+from aqt.deckchooser import DeckChooser
+from aqt import mw
+from aqt.utils import showInfo, getFile, showText
+from aqt.qt import *
+from anki.importing import TextImporter
+
+# some python libs
+import sys
+import tempfile
+import os
+import sqlite3
+import urllib2
+
+# addon's ui
+import kind2anki_ui
+
+from kindleimporter import KindleImporter
+
+
+class Kind2AnkiDialog(QDialog):
+    def __init__(self):
+        global mw
+        QDialog.__init__(self, mw, Qt.Window)
+        self.mw = mw
+        self.frm = kind2anki_ui.Ui_kind2ankiDialog()
+        self.frm.setupUi(self)
+        b = QPushButton(_("Import"))
+        self.frm.buttonBox.addButton(b, QDialogButtonBox.AcceptRole)
+        self.deck = DeckChooser(
+            self.mw, self.frm.deckArea, label=False)
+        self.frm.importMode.setCurrentIndex(
+                    self.mw.pm.profile.get('importMode', 1))
+
+        self.exec_()
+
+    def accept(self):
+        try:
+            db_path = getDBPath()
+            self.close()
+
+            target_language = self.frm.languageSelect.currentText()
+            kindleImporter = KindleImporter(db_path, target_language)
+
+            mw.progress.start(label=_("Processing..."), immediate=True)
+            showInfo("Translating words from database, it can take a while...")
+            kindleImporter.translateWordsFromDB()
+            mw.progress.finish()
+
+            temp_file_path = kindleImporter.createTemporaryFile()
+            self.setupImporter(temp_file_path)
+            self.selectDeck()
+
+            self.mw.progress.start(immediate=True, label="Importing...")
+            self.importer.run()
+            self.mw.progress.finish()
+
+            txt = _("Importing complete.") + "\n"
+            if self.importer.log:
+                txt += "\n".join(self.importer.log)
+            showText(txt)
+            os.remove(temp_file_path)
+        except urllib2.URLError:
+            showInfo("No Internet connection")
+        except IOError:
+            showInfo("DB file not selected, exiting")
+        except sqlite3.DatabaseError:
+            showInfo("Selected file is not a DB")
+        finally:
+            self.close()
+            mw.progress.finish()
+            self.mw.reset()
+
+    def setupImporter(self, temp_file_path):
+        self.importer = TextImporter(self.mw.col, unicode(temp_file_path))
+        self.importer.initMapping()
+        self.importer.importMode = self.frm.importMode.currentIndex()
+        self.mw.pm.profile['importMode'] = self.importer.importMode
+        self.importer.delimiter = ';'
+
+    def selectDeck(self):
+        did = self.deck.selectedId()
+        if did != self.importer.model['did']:
+            self.importer.model['did'] = did
+            self.mw.col.models.save(self.importer.model)
+        self.mw.col.decks.select(did)
+
+
+def getDBPath():
+    global mw
+    db_path = getFile(
+        mw, _("Select db file"), None, key="Import", filter="*.db")
+    if not db_path:
+        raise IOError
+    db_path = unicode(db_path)
+    return db_path
+
+action = QAction("kind2anki", mw)
+mw.connect(action, SIGNAL("triggered()"), Kind2AnkiDialog)
+mw.form.menuTools.addAction(action)
---
+++ kind2anki/kindleimporter.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+import sqlite3
+import sys
+import os
+import tempfile
+import codecs
+from aqt import mw
+
+# idea taken from Syntax Highlighting for Code addon, thanks!
+try:
+    # Try to find the modules in the global namespace:
+    import textblob
+    from textblob import TextBlob
+except:
+    # If not present, import modules from ./libs folder
+    sys.path.insert(0, os.path.join(mw.pm.addonFolder(), "kind2anki", "libs"))
+    import textblob
+    from textblob import TextBlob
+
+
+class KindleImporter():
+    def __init__(self, db_path, target_language):
+        self.db_path = db_path
+        self.target_language = target_language
+
+    def translateWordsFromDB(self):
+        self.words = self.getWordsFromDB()
+        self.translated = self.translateWords()
+
+    def getWordsFromDB(self):
+        conn = sqlite3.connect(self.db_path)
+        c = conn.cursor()
+        c.execute("SELECT word FROM WORDS")
+        words = c.fetchall()
+        words = [w[0] for w in words]
+        conn.close()
+        return words
+
+    def translateWords(self):
+        translated = []
+        for word in self.words:
+            try:
+                translated.append(
+                    TextBlob(word).translate(to=self.target_language))
+            except textblob.exceptions.NotTranslated:
+                translated.append('cannot translate')
+        return translated
+
+    def createTemporaryFile(self):
+        path = os.path.join(tempfile.gettempdir(), "kind2anki_temp.txt")
+        with codecs.open(path, "w", encoding="utf-8") as f:
+            for w, t in zip(self.words, self.translated):
+                f.write(u"{0};{1}\n".format(w, t))
+        return path

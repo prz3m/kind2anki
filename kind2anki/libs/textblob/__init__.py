7832590641b61b36e0764da1bc9449537cd34361
Mike Hardy <github@mikehardy.net>
2019-01-01 20:15:18 -0500

    Update TextBlob dependency from 0.11.1 to 0.15.2

--- kind2anki/libs/textblob/__init__.py
+++ kind2anki/libs/textblob/__init__.py
@@ -1,13 +1,11 @@
 import os
+from .blob import TextBlob, Word, Sentence, Blobber, WordList
 
-__version__ = '0.11.1'
+__version__ = '0.15.2'
 __license__ = 'MIT'
 __author__ = 'Steven Loria'
 
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# from .blob import TextBlob, Word, Sentence, Blobber, WordList
-from .blob import TextBlob
 
 __all__ = [
     'TextBlob',
